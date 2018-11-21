e377b54bd977cd9a4cc3048e80105f5d66ec2c0b
Brook Kynaston <brookkynaston@live.fr>
2018-11-21 19:00:48 +0000

    changed number display for pie chart on leads and appts, need to work out chnages for the other number displays

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -9,13 +9,13 @@ function makeGraphs(error, salesData) {
     salesData.forEach(function(d) {
         d.earned = parseInt(d.earned);
         d.lost = parseInt(d.lost);
-        d.leads = parseInt(d.leads_generated);
-        d.appointments_generated = parseInt(d.appointments_generated);
+        d.leads = parseInt(d.leads);
+        d.appointments = parseInt(d.appointments);
     })
 
     show_store_location_selector(ndx);
     show_amount_earned_per_store(ndx);
-    show_total_earn_and_loss(ndx);
+    show_earn_and_loss(ndx);
     show_leads_and_appts_per_store(ndx);
     show_number_display_leads_and_appts(ndx);
 
@@ -26,14 +26,12 @@ function makeGraphs(error, salesData) {
 
 function show_store_location_selector(ndx) {
     var stateDim = ndx.dimension(dc.pluck("store_location"));
-    var stateSelect = stateDim.group();
+    var stateGroup = stateDim.group();
 
     dc.selectMenu("#store-location-selector")
         .dimension(stateDim)
-        .group(stateSelect);
+        .group(stateGroup);
 }
-
-
 
 function show_amount_earned_per_store(ndx) {
 
@@ -92,17 +90,43 @@ function show_amount_earned_per_store(ndx) {
         .yAxis().tickFormat(function(v) {
             return "$" + v;
         })
-        
+
 
 }
 
-function show_total_earn_and_loss(ndx, location, element) {
+function show_earn_and_loss(ndx) {
 
     var stateDim = ndx.dimension(dc.pluck("store_location"));
     var total_earned_by_state = stateDim.group().reduceSum(dc.pluck("earned")); //DO NOT DELETE! THIS IS THE ONE THAT SHOWS INDIVIDUAL VALUES WHEN CLICKED
-    var total_lost_by_state = stateDim.group().reduceSum(dc.pluck("lost"));//DO NOT DELETE! THIS IS THE ONE THAT SHOWS INDIVIDUAL VALUES WHEN CLICKED
+    var total_lost_by_state = stateDim.group().reduceSum(dc.pluck("lost")); //DO NOT DELETE! THIS IS THE ONE THAT SHOWS INDIVIDUAL VALUES WHEN CLICKED
 
-    var percent_lost = 
+    var percent_lost = stateDim.group().reduce(
+        function add_item(p, v) {
+            p.count++;
+            p.earned += v.earned;
+            p.lost += v.lost;
+
+            return p;
+        },
+
+        function remove_item(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.earned = 0;
+                p.lost = 0;
+            }
+            else {
+                p.earned -= v.earned;
+                p.lost -= v.lost;
+
+            }
+            return p;
+        },
+
+        function initialise() {
+            return { count: 0, earned: 0, lost: 0 };
+        });
+
 
 
     dc.numberDisplay("#total-amount-earned")
@@ -113,26 +137,26 @@ function show_total_earn_and_loss(ndx, location, element) {
         .formatNumber(d3.format(".2"))
         .group(total_lost_by_state)
 
-   // dc.numberDisplay("#percent-loss")
-       // .formatNumber(d3.format(".2"))
-       // .group(percent_lost)
-       // .valueAccessor(function(d) { return d.value.total.toFixed(2); })
-
-
-
-
+    dc.numberDisplay("#percent-lost")
+        .formatNumber(d3.format(".0%"))
+        .group(percent_lost)
+        .valueAccessor(function(d) { //works! but get NaN for all other states appart from WASHINGTON (last)
+            if (d.count == 0) {
+                return 0;
+            }
+            else {
+                return (((d.value.lost / d.value.earned) * 100).toFixed(0)) / 100; // need to /100, as formatNumber seems to add two"00" when % is added
+            }
+        });
 }
 
 function show_leads_and_appts_per_store(ndx) {
 
     var stateDim = ndx.dimension(dc.pluck("store_location"));
 
-    var leadsPerState = stateDim.group().reduceSum(dc.pluck("leads_generated"));
+    var leadsPerState = stateDim.group().reduceSum(dc.pluck("leads"));
 
-    var apptsPerState = stateDim.group().reduceSum(dc.pluck("appointments_generated"));
-
-
-
+    var apptsPerState = stateDim.group().reduceSum(dc.pluck("appointments"));
 
     dc.barChart("#leads-and-appts-per-store")
         .width(800)
@@ -150,7 +174,8 @@ function show_leads_and_appts_per_store(ndx) {
         .elasticY(true)
         .renderHorizontalGridLines(true)
         .title(function(p) {
-            return ["Total generated in" + " " + p.key + " " + " is" + " " + p.value]})
+            return ["Total generated in" + " " + p.key + " " + " is" + " " + p.value]
+        })
         .xAxisLabel("State")
         .yAxisLabel("Total amount of leads and appointments generated")
         .yAxis().tickFormat(function(v) {
@@ -162,16 +187,72 @@ function show_number_display_leads_and_appts(ndx) {
 
     var stateDim = ndx.dimension(dc.pluck("store_location"));
 
-    var leadsPerState = stateDim.group().reduceSum(dc.pluck("leads_generated"));
+    var monthDim = ndx.dimension(dc.pluck("month"));
 
-    var apptsPerState = stateDim.group().reduceSum(dc.pluck("appointments_generated"));
+    var leads_to_appts = stateDim.group().reduce(
+        function add_item(p, v) {
+            p.count++;
+            p.leads += v.leads;
+            p.appts += v.appointments;
+            p.percent = (p.appts/p.leads)*100;
+            return p;
+        },
 
-    dc.numberDisplay("#total-leads")
-        .formatNumber(d3.format(".2")) //Showing same info as graph! should change to leads to appts %
-        .group(leadsPerState);
+        function remove_item(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.leads = 0;
+                p.appts = 0;
+                p.percent = 0;
+                
+            }
+            else {
+                p.leads -= v.leads;
+                p.appts -= v.appointments;
+                p.percent = (p.appts/p.leads)*100;
+            }
+            return p;
+        },
+
+        function initialise() {
+            return { count: 0, leads: 0, appts: 0, percent: 0 };
+        });
+    
+    
+
+    dc.pieChart("#leads-to-appts")
+        .width(400)
+        .height(400)
+        .radius(100)
+        .dimension(stateDim)
+        .group(leads_to_appts)
+        .valueAccessor(function(d) {
+            if (d.count == 0) {
+                return 0;
+            }
+            else {
+                return d.value.percent; //(((d.value.appts / d.value.leads) * 100).toFixed(0)) / 100; // need to /100, as formatNumber seems to add two"00" when % is added
+            }
+        })
+        .innerRadius(40)
+        .externalLabels(50)
+        .title(function(p) {
+            return ["Average leads to appointments in" + " " + p.key + " " + " is" + " " + p.value.percent.toFixed(0) + "%"]
+        })
+        .transitionDuration(1000)
+        .ordinalColors(['red', '#blue', 'green', 'yellow', 'black', 'pink'])
+        ;
+        
+    
+        
+        
 
 
-    dc.numberDisplay("#total-appts")
-        .formatNumber(d3.format(".2")) 
-        .group(apptsPerState);
+
+   
+        
+
+
+
+
 }
