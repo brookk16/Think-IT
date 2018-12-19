335073b06271369577a2f880fa7f8d21a4ac5169
Brook Kynaston <brookkynaston@live.fr>
2018-12-19 19:46:19 +0000

    hover style added

---
+++ assets/js/hover.js
@@ -0,0 +1,18 @@
+//Hover code was taken and modified from https://jqueryui.com/tooltip/#custom-style
+
+ $(function() {
+        $(document).tooltip({
+            position: {
+                my: "center bottom-20",
+                at: "center top",
+                using: function(position, feedback) {
+                    $(this).css(position);
+                    $("<div>")
+                        .addClass("arrow")
+                        .addClass(feedback.vertical)
+                        .addClass(feedback.horizontal)
+                        .appendTo(this);
+                }
+            }
+        });
+    });
